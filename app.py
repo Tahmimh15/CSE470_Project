749dacae68cb8ba66a3c5378015b32e634de9e76
Ehmaokner <mdemonhossain09876@gmail.com>
2023-12-06 22:31:49 +0600

    commit

--- app.py
+++ app.py
@@ -1,28 +1,16 @@
-
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask import Flask, render_template
-
-app = Flask(__name__, static_url_path='/static')
-#arrr= dfceffv
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  
+app.secret_key = 'your_secure_secret_key'  # Replace with an actual secure secret key
 
-users = [
-    {'email': 'user1@example.com', 'password': 'pass1', 'type': 'premium'},
-    {'email': 'user2@example.com', 'password': 'pass2', 'type': 'public'},
-]
+# Dummy user data (replace with your authentication logic)
+users = {
+    'user1@example.com': {'password': 'password1', 'type': 'premium'},
+    'user2@example.com': {'password': 'password2', 'type': 'public'},
+}
 
 @app.route('/')
 def index():
-    if 'user' in session:
-        return render_template('home.html', user=session['user'])
     return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
@@ -31,19 +19,26 @@ def login():
     password = request.form.get('password')
     login_type = request.form.get('login-type')
 
-    # Check if the user exists and the password is correct
-    user = next((user for user in users if user['email'] == email and user['password'] == password), None)
+    if email in users and users[email]['password'] == password:
+        session['user'] = {
+            'email': email,
+            'type': login_type,
+        }
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html', error='Invalid credentials')
 
-    if user and user['type'] == login_type:
-        session['user'] = user
+@app.route('/home')
+def home():
+    user = session.get('user')
+    if user:
+        return render_template('home.html', user=user)
+    else:
         return redirect(url_for('index'))
 
-    return "Invalid credentials or login type."
-
-@app.route('/logout')
-def logout():
-    session.pop('user', None)
-    return redirect(url_for('index'))
+@app.route('/register')
+def register():
+    return render_template('registration.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
